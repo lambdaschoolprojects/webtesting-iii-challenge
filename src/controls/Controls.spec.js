5cc3034e7f6741c96d85caaa9a9e7dad746ee055
Rane <rane@blasph.me>
2019-06-25 20:14:28 -0700

    Controls and Dashboard initial tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,34 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'
+
+import Controls from './Controls';
+
+it("renders without error", () => {
+    render(<Controls/>);
+});
+
+it("calls function when button clicked", () => {
+    const toggleLocked = jest.fn();
+    const toggleClosed = jest.fn();
+
+    const props = {
+        closed: true,
+        locked: false,
+        toggleLocked: toggleLocked,
+        toggleClosed: toggleClosed
+    }
+    const { getByText } = render(<Controls {...props}/>);
+    const tlButton = getByText(/lock gate/i);
+
+    fireEvent.click(tlButton);
+
+    expect(toggleLocked).toHaveBeenCalled();
+
+    const tcButton = getByText(/open gate/i);
+
+    fireEvent.click(tcButton);
+
+    expect(toggleClosed).toHaveBeenCalled();
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import { render, fireEvent, getNodeText } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+it("renders without error", () => {
+    render(<Dashboard />);
+});
+
+it("starts with gate unlocked and open", () => {
+    const { getByTestId } = render(<Dashboard />);
+    const lockStatus = getByTestId("lock-status");
+    const openStatus = getByTestId("open-status");
+
+    expect(getNodeText(lockStatus).toLowerCase()).toBe("unlocked");
+    expect(getNodeText(openStatus).toLowerCase()).toBe("open");
+});
